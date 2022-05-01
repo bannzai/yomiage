7ccd6cf6497c266bba2924e5d0c82b676e5dcb8a
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 08:19:34 +0900

    Initial commit

---
+++ Yomiage/Articles/ArticlesPage.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct ArticlesPage: View {
+    var body: some View {
+        List {
+            Group {
+
+                VStack {
+
+                }
+            }
+            .listRowInsets(EdgeInsets())
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(.plain)
+    }
+}
+
+
+struct ArticlesRow: View {
+    var body: some View {
+        HStack {
+            AsyncImage(url: <#T##URL?#>)
+            VStack {
+
+            }
+        }
+    }
+}
---
+++ Yomiage/YomiageApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomiageApp.swift
+//  Yomiage
+//
+//  Created by 廣瀬雄大 on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct YomiageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
