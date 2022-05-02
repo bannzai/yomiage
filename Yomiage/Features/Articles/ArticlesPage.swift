2f74c780b55d4d3e0b82b204616ef56f2aabbf06
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 21:08:44 +0900

    Fix indent

--- Yomiage/Features/Articles/ArticlesPage.swift
+++ Yomiage/Features/Articles/ArticlesPage.swift
@@ -1,82 +1,82 @@
 import SwiftUI
 
 struct ArticlesPage: View {
-    @Environment(\.articleDatastore) var articleDatastore
-    @State private var addArticleSheetIsPresented = false
+  @Environment(\.articleDatastore) var articleDatastore
+  @State private var addArticleSheetIsPresented = false
 
-    var body: some View {
-        StreamView(stream: articleDatastore.articlesStream()) { articles in
-            if articles.isEmpty {
-                // TODO: 
-                EmptyView()
-            } else {
-                List {
-                    ForEach(articles) { article in
-                        switch article.typedKind {
-                        case .note:
-                            NoteArticle(article: article, noteArticle: article.note)
-                        case .medium:
-                            // TODO:
-                            EmptyView()
-                        case nil:
-                            EmptyView()
-                        }
-                    }
-                    .listRowInsets(EdgeInsets())
-                    .listRowSeparator(.hidden)
-                }
-                .listStyle(.plain)
+  var body: some View {
+    StreamView(stream: articleDatastore.articlesStream()) { articles in
+      if articles.isEmpty {
+        // TODO:
+        EmptyView()
+      } else {
+        List {
+          ForEach(articles) { article in
+            switch article.typedKind {
+            case .note:
+              NoteArticle(article: article, noteArticle: article.note)
+            case .medium:
+              // TODO:
+              EmptyView()
+            case nil:
+              EmptyView()
             }
-        } errorContent: { error, reload in
-            UniversalErrorView(error: error, reload: reload)
-        } loading: {
-            ProgressView()
+          }
+          .listRowInsets(EdgeInsets())
+          .listRowSeparator(.hidden)
         }
-        .toolbar(content: {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    addArticleSheetIsPresented = true
-                } label: {
-                    Image(systemName: "plus")
-                        .imageScale(.large)
-                        .foregroundColor(Color(.label))
-                        .frame(width: 28, height: 28)
-                }
-            }
-        })
-        .sheet(isPresented: $addArticleSheetIsPresented, detents: [.medium()]) {
-            AddArticleSheet()
-        }
+        .listStyle(.plain)
+      }
+    } errorContent: { error, reload in
+      UniversalErrorView(error: error, reload: reload)
+    } loading: {
+      ProgressView()
     }
+    .toolbar(content: {
+      ToolbarItem(placement: .navigationBarTrailing) {
+        Button {
+          addArticleSheetIsPresented = true
+        } label: {
+          Image(systemName: "plus")
+            .imageScale(.large)
+            .foregroundColor(Color(.label))
+            .frame(width: 28, height: 28)
+        }
+      }
+    })
+    .sheet(isPresented: $addArticleSheetIsPresented, detents: [.medium()]) {
+      AddArticleSheet()
+    }
+  }
 }
 
 
 struct NoteArticle: View {
-    let article: Article
-    let noteArticle: Article.Note?
+  let article: Article
+  let noteArticle: Article.Note?
 
-    var body: some View {
-        if let noteArticle = noteArticle {
-            HStack {
-                if let eyeCatchImageURL = noteArticle.eyeCatchImageURL,
-                   let url = URL(string: eyeCatchImageURL) {
-                    AsyncImage(url: url) { image in
-                        image
-                            .resizable()
-                            .frame(width: 60, height: 60)
-                    } placeholder: {
-                        ProgressView()
-                            .frame(width: 60, height: 60)
-                    }
-                }
-
-                VStack {
-                    Text(noteArticle.title)
-                        .font(.system(.headline))
-                    Text(noteArticle.author)
-                        .font(.system(.caption))
-                }
-            }
+  var body: some View {
+    if let noteArticle = noteArticle {
+      HStack {
+        if let eyeCatchImageURL = noteArticle.eyeCatchImageURL,
+           let url = URL(string: eyeCatchImageURL) {
+          AsyncImage(url: url) { image in
+            image
+              .resizable()
+              .frame(width: 60, height: 60)
+          } placeholder: {
+            ProgressView()
+              .frame(width: 60, height: 60)
+          }
         }
+
+        VStack {
+          Text(noteArticle.title)
+            .font(.system(.headline))
+          Text(noteArticle.author)
+            .font(.system(.caption))
+        }
+      }
     }
+  }
 }
