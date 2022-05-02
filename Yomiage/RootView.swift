bb879d35cfeeffae59580d1e42a6927ce3199108
bannzai <kingkong999yhirose@gmail.com>
2022-05-03 07:20:43 +0900

    Fix indent

--- Yomiage/RootView.swift
+++ Yomiage/RootView.swift
@@ -1,40 +1,40 @@
 import SwiftUI
 
 struct RootView: View {
-    @Environment(\.auth) private var auth
-    @StateObject var screenStateNotifier = ScreenStateNotifier()
+  @Environment(\.auth) private var auth
+  @StateObject var screenStateNotifier = ScreenStateNotifier()
 
-    @State private var signInError: Error?
+  @State private var signInError: Error?
 
-    var body: some View {
-        NavigationView {
-            if let signInError = signInError {
-                UniversalErrorView(error: signInError, reload: logIn)
-            } else {
-                switch screenStateNotifier.state {
-                case .waiting:
-                    ProgressView()
-                        .onAppear(perform: logIn)
-                case .main:
-                    ArticlesPage()
-                }
-            }
+  var body: some View {
+    NavigationView {
+      if let signInError = signInError {
+        UniversalErrorView(error: signInError, reload: logIn)
+      } else {
+        switch screenStateNotifier.state {
+        case .waiting:
+          ProgressView()
+            .onAppear(perform: logIn)
+        case .main:
+          ArticlesPage()
         }
-        .onAppear(perform: screenStateNotifier.launch)
-        .onReceive(screenStateNotifier.$state) { state in
-            if case let .main(user) = state {
-                UserDatabase.shared.setUserID(user.uid)
-            }
-        }
+      }
     }
-
-    private func logIn() {
-        Task { @MainActor in
-            do {
-                _ = try await auth.signInOrCachedUser()
-            } catch {
-                signInError = error
-            }
-        }
+    .onAppear(perform: screenStateNotifier.launch)
+    .onReceive(screenStateNotifier.$state) { state in
+      if case let .main(user) = state {
+        UserDatabase.shared.setUserID(user.uid)
+      }
     }
+  }
+
+  private func logIn() {
+    Task { @MainActor in
+      do {
+        _ = try await auth.signInOrCachedUser()
+      } catch {
+        signInError = error
+      }
+    }
+  }
 }
