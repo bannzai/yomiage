0e429676ca75854ec33430d58dc40774afefc3a8
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 10:56:10 +0900

    Add AppDelegate

---
+++ Yomiage/AppDelegate.swift
@@ -0,0 +1,27 @@
+import UIKit
+import Firebase
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupFirebase()
+        return true
+    }
+}
+
+// MARK: - Private
+private extension AppDelegate {
+    func setupFirebase() {
+        let firebaseFileName: String
+#if DEBUG
+        firebaseFileName = "GoogleService-Info-dev"
+#else
+        firebaseFileName = "GoogleService-Info-prod"
+#endif
+
+        let path = Bundle.main.path(forResource: firebaseFileName, ofType: "plist")!
+        let options = FirebaseOptions(contentsOfFile: path)!
+
+        FirebaseApp.configure(options: options)
+    }
+
+}
