7a7463d6591c6eb40748153f77f791321974a629
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 21:49:27 +0900

    Missed

--- Yomiage/Component/Sheet.swift
+++ Yomiage/Component/Sheet.swift
@@ -1,9 +1,6 @@
 import SwiftUI
 
 struct Sheet<Content>: UIViewRepresentable where Content: View {
-  @Binding var isPresented: Bool
-
-  var onDismiss: (() -> Void)? = nil
   let detents: [UISheetPresentationController.Detent]
   @ViewBuilder let content: Content
 
@@ -12,12 +9,7 @@ struct Sheet<Content>: UIViewRepresentable where Content: View {
   }
 
   func updateUIView(_ uiView: UIView, context: Context) {
-    let hostingController = HostingController(rootView: content)
-    hostingController.onDisappear = {
-      isPresented = false
-      onDismiss?()
-    }
-
+    let hostingController = UIHostingController(rootView: content)
     if let sheetController = hostingController.sheetPresentationController {
       sheetController.detents = detents
       sheetController.prefersGrabberVisible = true
@@ -25,42 +17,10 @@ struct Sheet<Content>: UIViewRepresentable where Content: View {
       sheetController.largestUndimmedDetentIdentifier = .medium
     }
 
-    hostingController.presentationController?.delegate = context.coordinator
-
-    if isPresented {
-      uiView.window?.rootViewController?.present(hostingController, animated: true)
-    } else {
-      uiView.window?.rootViewController?.dismiss(animated: true)
-    }
+    uiView.window?.rootViewController?.present(hostingController, animated: true)
   }
 
-  func makeCoordinator() -> Coordinator {
-    Coordinator(isPresented: $isPresented, onDismiss: onDismiss)
-  }
-
-  class Coordinator: NSObject, UISheetPresentationControllerDelegate {
-    @Binding var isPresented: Bool
-    let onDismiss: (() -> Void)?
-
-    init(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil) {
-      self._isPresented = isPresented
-      self.onDismiss = onDismiss
-    }
-
-    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
-      isPresented = false
-      onDismiss?()
-    }
-  }
-}
-
-
-// Workaround Handle @Environment(\.dismiss)
-private class HostingController<Content: View>: UIHostingController<Content> {
-  var onDisappear: (() -> Void)!
-
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    onDisappear()
+  func makeCoordinator() -> Void {
+    ()
   }
 }
