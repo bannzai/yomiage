4ee46673e2867a88c2d97b53719083ecbdf53520
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 16:21:10 +0900

    Rename to RootView

---
+++ Yomiage/RootView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct RootView: View {
+    @Environment(\.auth) private var auth
+    @StateObject var screenStateNotifier = ScreenStateNotifier()
+
+    @State private var signInError: Error?
+
+    var body: some View {
+        NavigationView {
+            if let signInError = signInError {
+                UniversalErrorView(error: signInError, reload: signIn)
+            } else {
+                switch screenStateNotifier.state {
+                case .waiting:
+                    ProgressView()
+                        .onAppear(perform: signIn)
+                case .main:
+                    ArticlesPage()
+                }
+            }
+        }
+    }
+
+    private func signIn() {
+        Task { @MainActor in
+            do {
+                _ = try await auth.signIn()
+            } catch {
+                signInError = error
+            }
+        }
+    }
+}
