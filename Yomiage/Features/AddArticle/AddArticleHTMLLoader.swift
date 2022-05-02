e734a1835155c1d4f7f98442372a71358aa69a13
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 21:09:23 +0900

    Fix

--- Yomiage/Features/AddArticle/AddArticleHTMLLoader.swift
+++ Yomiage/Features/AddArticle/AddArticleHTMLLoader.swift
@@ -2,81 +2,91 @@ import SwiftUI
 import Kanna
 
 final class AddArticleHTMLLoader: ObservableObject, LoadHTMLLoader {
-    @Environment(\.articleDatastore) private var articleDatastore
+  @Environment(\.articleDatastore) private var articleDatastore
 
-    typealias Target = (url: URL, Article.Kind)
-    @Published private(set) var target: Target?
-    @Published private(set) var error: LocalizedError?
+  typealias Target = (url: URL, kind: Article.Kind)
+  @Published private(set) var target: Target?
+  @Published private(set) var localizedError: Error?
+  @Published private(set) var loadedArticle: Article?
 
-    func load(url: URL) {
-        switch url.host {
-        case "note.com":
-            target = (url, .note)
-        case "medium.com":
-            target = (url, .medium)
-        case _:
-            error = HostMismatchError()
-        }
+  func load(url: URL) {
+    switch url.host {
+    case "note.com":
+      target = (url, .note)
+    case "medium.com":
+      target = (url, .medium)
+    case _:
+      localizedError = HostMismatchError()
+    }
+  }
+
+  func handlEevaluateJavaScript(arguments: (Any?, Error?)) {
+    guard let target = target else {
+      return
     }
 
-    func handlEevaluateJavaScript(arguments: (Any?, Error?)) {
-        guard let target = target else {
-            return
-        }
-
-        defer {
-            self.target = nil
-        }
-
-        if let html = arguments.0 as? String {
-            proceedRead(html: html, target: target)
-        } else if let loadError = arguments.1 {
-            error = WebViewLoadHTMLError(error: loadError)
-        } else {
-            error = WebViewLoadHTMLError(error: nil)
-        }
+    defer {
+      self.target = nil
     }
 
-    private func proceedRead(html: String, target: Target) {
-        if let doc = try? HTML(html: html, encoding: .utf8) {
-            print(doc.title)
-
-            // Search for nodes by CSS
-            for link in doc.css("a, link") {
-                print(link.text)
-                print(link["href"])
-            }
-
-            // Search for nodes by XPath
-            for link in doc.xpath("//a | //link") {
-                print(link.text)
-                print(link["href"])
-            }
-        }
+    if let html = arguments.0 as? String {
+      proceedRead(html: html, target: target)
+    } else if let loadError = arguments.1 {
+      localizedError = WebViewLoadHTMLError(error: loadError)
+    } else {
+      localizedError = WebViewLoadHTMLError(error: nil)
     }
+  }
+
+  private func proceedRead(html: String, target: Target) {
+    do {
+      let doc = try HTML(html: html, encoding: .utf8)
+      switch target.kind {
+      case .note:
+        if let title = doc.at_xpath(#"//*[@id="__layout"]/div/div[1]/div[2]/main/div[1]/article/div[1]/div/div/h1"#)?.text?.trimmed,
+           let author = doc.at_xpath(#"//*[@id="__layout"]/div/div[1]/div[2]/main/div[1]/article/div[1]/div/div/div[2]/div/div[1]/div/a"#)?.text?.trimmed {
+          let eyeCatchImageURL = doc.at_xpath(#"//*[@id="__layout"]/div/div[1]/div[2]/main/div[1]/article/div[1]/div/div/figure/a/img"#)?.text?.trimmed
+          loadedArticle = .init(
+            kind: Article.Kind.note.rawValue,
+            note: .init(
+              title: title,
+              pageURL: target.url.absoluteString,
+              author: author,
+              eyeCatchImageURL: eyeCatchImageURL,
+              createdDate: .init()
+            )
+          )
+        }
+      case .medium:
+        fatalError()
+      }
+    } catch {
+      localizedError = WebViewLoadHTMLError(error: error)
+    }
+  }
 }
 
 
 fileprivate struct HostMismatchError: LocalizedError {
-    var errorDescription: String? {
-        "対応していないURLです"
-    }
-    var failureReason: String? {
-        "note.com,medium.comで記事が存在するURLを入力してください"
-    }
-    let helpAnchor: String? = nil
-    let recoverySuggestion: String? = nil
+  var errorDescription: String? {
+    "対応していないURLです"
+  }
+  var failureReason: String? {
+    "note.com,medium.comで記事が存在するURLを入力してください"
+  }
+  let helpAnchor: String? = nil
+  let recoverySuggestion: String? = nil
 }
 
 fileprivate struct WebViewLoadHTMLError: LocalizedError {
-    let error: Error?
+  let error: Error?
 
-    var errorDescription: String? {
-        "再度読み込みをしてください"
-    }
-    var failureReason: String? {
-        error?.localizedDescription ?? "読み込みに失敗しました"
-    }
-    let helpAnchor: String? = nil
-    let recoverySuggestion: String? = nil
+  var errorDescription: String? {
+    "再度読み込みをしてください"
+  }
+  var failureReason: String? {
+    error?.localizedDescription ?? "読み込みに失敗しました"
+  }
+  let helpAnchor: String? = nil
+  let recoverySuggestion: String? = nil
 }
--- Yomiage/Features/AddArticle/AddArticleSheet.swift
+++ Yomiage/Features/AddArticle/AddArticleSheet.swift
@@ -1,31 +1,70 @@
 import SwiftUI
 
 struct AddArticleSheet: View {
-    @StateObject private var loader = AddArticleHTMLLoader()
-    @State private var text: String = ""
-    private var url: URL? { .init(string: text) }
+  @Environment(\.dismiss) private var dismiss
+  @Environment(\.articleDatastore) private var articleDatastore
 
-    var body: some View {
-        ZStack {
-            if let url = loader.target?.url {
-                LoadHTMLWebView(url: url, loader: loader)
-            }
-            VStack {
-                TextField("https://", text: $text)
+  @StateObject private var loader = AddArticleHTMLLoader()
+  @State private var text: String = ""
+  @State private var error: Error?
 
-                Button {
-                    if let url = url {
-                        loader.load(url: url)
-                    }
-                } label: {
-                    Text("追加する")
-                }
-                .buttonStyle(.primary)
-                .disabled(url == nil || loader.target != nil)
-            }
-            .frame(alignment: .top)
-            .padding(.vertical, 20)
+  var body: some View {
+    ZStack {
+      if let url = loader.target?.url {
+        LoadHTMLWebView(url: url, loader: loader)
+      }
+
+      VStack {
+        TextField("https://", text: $text)
+
+        Button {
+          if let url = url {
+            loader.load(url: url)
+          }
+        } label: {
+          Text("追加する")
         }
+        .buttonStyle(.primary)
+        .disabled(url == nil || loader.target != nil)
+      }
+      .frame(alignment: .top)
+      .padding(.vertical, 20)
+
+      if loader.target != nil {
+        HUD()
+      }
     }
+    .onReceive(loader.$loadedArticle) { article in
+      if let article = article {
+        Task { @MainActor in
+          do {
+            try await articleDatastore.create(article: article)
+            dismiss()
+          } catch {
+            self.error = error
+          }
+        }
+      }
+    }
+    .onReceive(loader.$localizedError, perform: { error in
+      self.error = error
+    })
+    .errorAlert(error: $error)
+  }
+
+  private var url: URL? { .init(string: text) }
 }
 
+
+private struct AddArticleError: LocalizedError {
+  let error: Error?
+
+  var errorDescription: String? {
+    "記事の登録に失敗しました"
+  }
+  var failureReason: String? {
+    error?.localizedDescription
+  }
+  var recoverySuggestion: String?
+  var helpAnchor: String?
+}
