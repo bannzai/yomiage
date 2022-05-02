0e5fd2b194960bd5850cdb246914ec6cfedb3879
bannzai <kingkong999yhirose@gmail.com>
2022-05-03 07:20:59 +0900

    fix indent

--- Yomiage/Services/Auth/Auth.swift
+++ Yomiage/Services/Auth/Auth.swift
@@ -6,64 +6,64 @@ import os.log
 private let authLogger = Logger(subsystem: "com.bannzai.yomiage", category: "Auth")
 
 public struct Auth {
-    private var client: FirebaseAuth.Auth { .auth() }
+  private var client: FirebaseAuth.Auth { .auth() }
 
-    public typealias User = FirebaseAuth.User
+  public typealias User = FirebaseAuth.User
 
-    public static var firebaseCurrentUser: User? {
-        FirebaseAuth.Auth.auth().currentUser
+  public static var firebaseCurrentUser: User? {
+    FirebaseAuth.Auth.auth().currentUser
+  }
+
+  // MARK: - Stream
+  public func stateDidChange() -> AsyncStream<User?> {
+    .init { continuation in
+      client.addStateDidChangeListener { auth, user in
+        authLogger.debug("[DEBUG] auth state did changed. user is not nil: \(user != nil), user id \(String(describing: user?.uid))")
+        continuation.yield(user)
+      }
+    }
+  }
+
+  // MARK: - SignIn
+  public func signInOrCachedUser() async throws -> User {
+    if let user = client.currentUser {
+      return user
     }
 
-    // MARK: - Stream
-    public func stateDidChange() -> AsyncStream<User?> {
-        .init { continuation in
-            client.addStateDidChangeListener { auth, user in
-                authLogger.debug("[DEBUG] auth state did changed. user is not nil: \(user != nil), user id \(String(describing: user?.uid))")
-                continuation.yield(user)
+    return try await withCheckedThrowingContinuation { continuation in
+      if let user = client.currentUser {
+        authLogger.debug("[DEBUG] SignIn: user is already exists")
+        continuation.resume(returning: user)
+      } else {
+        client.signInAnonymously() { (result, error) in
+          if let error = error {
+            authLogger.debug("[DEBUG] SignIn: got error")
+            continuation.resume(throwing: (error))
+          } else {
+            guard let result = result else {
+              fatalError("unexpected pattern about result and error is nil")
             }
+            authLogger.debug("[DEBUG] SignIn: SignIn: Done signin via signInAnonymously")
+            continuation.resume(returning: result.user)
+          }
         }
+      }
     }
-
-    // MARK: - SignIn
-    public func signInOrCachedUser() async throws -> User {
-        if let user = client.currentUser {
-            return user
-        }
-
-        return try await withCheckedThrowingContinuation { continuation in
-            if let user = client.currentUser {
-                authLogger.debug("[DEBUG] SignIn: user is already exists")
-                continuation.resume(returning: user)
-            } else {
-                client.signInAnonymously() { (result, error) in
-                    if let error = error {
-                        authLogger.debug("[DEBUG] SignIn: got error")
-                        continuation.resume(throwing: (error))
-                    } else {
-                        guard let result = result else {
-                            fatalError("unexpected pattern about result and error is nil")
-                        }
-                        authLogger.debug("[DEBUG] SignIn: SignIn: Done signin via signInAnonymously")
-                        continuation.resume(returning: result.user)
-                    }
-                }
-            }
-        }
-    }
+  }
 }
 
 public struct AuthEnvironmentKey: SwiftUI.EnvironmentKey {
-    public static var defaultValue: Auth = .init()
+  public static var defaultValue: Auth = .init()
 }
 
 extension EnvironmentValues {
-    public var auth: Auth {
-        get {
-            self[AuthEnvironmentKey.self]
-        }
-        set {
-            self[AuthEnvironmentKey.self] = newValue
-        }
+  public var auth: Auth {
+    get {
+      self[AuthEnvironmentKey.self]
     }
+    set {
+      self[AuthEnvironmentKey.self] = newValue
+    }
+  }
 }
 
