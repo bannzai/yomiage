3f08962e4b9aea33799b53c89ed474738c18b541
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 12:49:14 +0900

    Fix datastore

--- Yomiage/Datastore/ArticleDatastore.swift
+++ Yomiage/Datastore/ArticleDatastore.swift
@@ -1,11 +1,19 @@
-import Foundation
+import SwiftUI
 
 struct ArticleDatastore {
-    private init() { }
-    static let shared = ArticleDatastore()
-    
     // TODO: Pagenation
     func articlesStream() -> AsyncThrowingStream<[Article], Error> {
         UserDatabase.shared.articlesReference().stream()
+    }
+}
+
+struct ArticleDatastoreEnvironmentKey: EnvironmentKey {
+    static var defaultValue: ArticleDatastore = .init()
+}
+
+extension EnvironmentValues {
+    var articleDatastore: ArticleDatastore {
+        get { self[ArticleDatastoreEnvironmentKey.self] }
+        set { self[ArticleDatastoreEnvironmentKey.self] = newValue }
     }
 }
