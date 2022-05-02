dbee62ac7ecf267c4403701b4b19903619426881
bannzai <kingkong999yhirose@gmail.com>
2022-05-03 07:23:09 +0900

    Fix indent

--- Yomiage/Style/Color.swift
+++ Yomiage/Style/Color.swift
@@ -1,8 +1,9 @@
 import SwiftUI
 
 extension Color {
-    // DE396B
-    static let primary = Color(red: 222 / 255, green: 57 / 255, blue: 107 / 255)
+  // DE396B
+  static let primary = Color(red: 222 / 255, green: 57 / 255, blue: 107 / 255)
 
+  
 }
 
