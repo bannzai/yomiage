b0f243c422771e311c12df35af8462414ab4f0a6
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 20:31:33 +0900

    Fix Page

--- Yomiage/Features/AddArticle/AddArticleSheet.swift
+++ Yomiage/Features/AddArticle/AddArticleSheet.swift
@@ -6,22 +6,26 @@ struct AddArticleSheet: View {
     private var url: URL? { .init(string: text) }
 
     var body: some View {
-        VStack {
-            TextField("https://", text: $text)
-
-            Button {
-                if let url = url {
-                    loader.load(url: url)
-                }
-            } label: {
-                Text("追加する")
+        ZStack {
+            if let url = loader.target?.url {
+                LoadHTMLWebView(url: url, loader: loader)
             }
-            .buttonStyle(.primary)
-            .disabled(url == nil)
-        }
-        .frame(alignment: .top)
-        .padding(.vertical, 20)
-    }
+            VStack {
+                TextField("https://", text: $text)
 
+                Button {
+                    if let url = url {
+                        loader.load(url: url)
+                    }
+                } label: {
+                    Text("追加する")
+                }
+                .buttonStyle(.primary)
+                .disabled(url == nil || loader.target != nil)
+            }
+            .frame(alignment: .top)
+            .padding(.vertical, 20)
+        }
+    }
 }
 
