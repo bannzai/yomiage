e2db3b3eea02fe206d42114251b226b8a5cdea0f
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 12:19:34 +0900

    Add ArticleDatastore

---
+++ Yomiage/Datastore/ArticleDatastore.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+struct ArticleDatastore {
+    // TODO: Pagenation
+    func articlesStream() -> AsyncThrowingStream<[Article], Error> {
+        UserDatabase.shared.articlesReference().stream()
+    }
+}
