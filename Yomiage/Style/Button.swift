dbb2ba7554e4f70573bdd8e3d266a19715c12b5c
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 12:48:05 +0900

    Add primary button style

---
+++ Yomiage/Style/Button.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) var isEnabled
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label.padding(.horizontal, 20)
+            .padding(.vertical, 16)
+            .background(
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(isEnabled ? Color.primary : Color(.systemGray3))
+            )
+            .opacity(configuration.isPressed ? 0.7 : 1.0)
+    }
+}
+
+extension ButtonStyle where Self == PrimaryButtonStyle {
+    static var primary: PrimaryButtonStyle { PrimaryButtonStyle() }
+}
