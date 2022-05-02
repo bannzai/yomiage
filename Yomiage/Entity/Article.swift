437c42956ab735664d9e317621ef4ca26b6f97a8
bannzai <kingkong999yhirose@gmail.com>
2022-05-02 11:13:20 +0900

    Fix entity

--- Yomiage/Entity/Article.swift
+++ Yomiage/Entity/Article.swift
@@ -1,5 +1,35 @@
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct Article: Decodable {
-    let id: String
+    @DocumentID var id: String?
+
+    let kind: String
+    let note: Note?
+    let medium: Medium?
+
+    // MARK: - Kind
+    var typedKind: Kind? { .init(rawValue: kind) }
+    enum Kind: String {
+        case note
+        case medium
+    }
+
+    // MARK: - Each Kind Structure
+    struct Note: Codable {
+        let title: String
+        let pageURL: String
+        let author: String
+        let eyeCatchImageURL: String?
+        let createdDate: Timestamp
+    }
+
+    struct Medium: Codable {
+        let title: String
+        let pageURL: String
+        let author: String
+        let eyeCatchImageURL: String?
+        let createdDate: Timestamp
+    }
 }
