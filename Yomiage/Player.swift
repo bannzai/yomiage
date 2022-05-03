36eff16aa5eb215f97c80873927f2c6369fd6976
bannzai <kingkong999yhirose@gmail.com>
2022-05-03 18:46:27 +0900

    Add progress

--- Yomiage/Player.swift
+++ Yomiage/Player.swift
@@ -2,22 +2,20 @@ import Combine
 import SwiftUI
 import AVFoundation
 
-final class Player: ObservableObject {
+final class Player: NSObject, ObservableObject {
   @Published var volume = UserDefaults.standard.float(forKey: UserDefaultsKeys.playerVolume)
   @Published var rate = UserDefaults.standard.float(forKey: UserDefaultsKeys.playerRate)
   @Published var pitch = UserDefaults.standard.float(forKey: UserDefaultsKeys.playerPitch)
 
+  private var speakingText: String?
+  private var progress: Progress?
+
   let synthesizer = AVSpeechSynthesizer()
   var canceller: Set<AnyCancellable> = []
 
-  init() {
-    bind()
-    configure()
-  }
-}
+  override init() {
+    super.init()
 
-private extension Player {
-  func bind() {
     $volume.sink { volume in
       UserDefaults.standard.set(volume, forKey: UserDefaultsKeys.playerVolume)
     }.store(in: &canceller)
@@ -29,17 +27,30 @@ private extension Player {
     }.store(in: &canceller)
 
     objectWillChange.sink { [weak self] in
-      self?.configure()
+      self?.reset()
     }.store(in: &canceller)
+
+    synthesizer.delegate = self
   }
 
-  func configure() {
-    let utterance = AVSpeechUtterance()
+  func speak(text: String) {
+    let utterance = AVSpeechUtterance(string: text)
     utterance.volume = volume
     utterance.rate = rate
     utterance.pitchMultiplier = pitch
 
+    speakingText = text
+
     synthesizer.speak(utterance)
+  }
+}
+
+private extension Player {
+  func reset() {
+//    if synthesizer.isSpeaking {
+//      synthesizer.stopSpeaking(at: <#T##AVSpeechBoundary#>)
+//    }
+//    speak(text: String)
   }
 }
 
@@ -63,3 +74,18 @@ extension Player {
   }
 }
 
+extension Player: AVSpeechSynthesizerDelegate {
+  private struct Progress {
+    var range: Range<String.Index>
+    var lastWord: String
+    var speechString: String
+  }
+  func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+    guard let range = Range(characterRange, in: utterance.speechString) else {
+      return
+    }
+
+    let lastWord = utterance.speechString[range]
+    progress = .init(range: range, lastWord: String(lastWord), speechString: utterance.speechString)
+  }
+}
