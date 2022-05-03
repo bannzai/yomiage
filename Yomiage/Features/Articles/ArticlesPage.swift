b4745ea9c54fd69b7f53cfc8cc2c57d1830637dd
bannzai <kingkong999yhirose@gmail.com>
2022-05-04 08:06:13 +0900

    Fix layout

--- Yomiage/Features/Articles/ArticlesPage.swift
+++ Yomiage/Features/Articles/ArticlesPage.swift
@@ -94,74 +94,67 @@ struct NoteArticle: View {
 
   var body: some View {
     if let noteArticle = noteArticle {
-      ZStack {
-        if let article = loader.loadingArticle?.note, let url = URL(string: article.pageURL) {
-          LoadHTMLWebView(url: url, loader: loader)
+      HStack {
+        Group {
+          if let eyeCatchImageURL = noteArticle.eyeCatchImageURL,
+             let url = URL(string: eyeCatchImageURL) {
+            AsyncImage(url: url) { image in
+              image
+                .resizable()
+                .scaledToFill()
+            } placeholder: {
+              ProgressView()
+            }
+          } else {
+            Image(systemName: "photo")
+          }
+        }
+        .frame(width: 60, height: 60)
+        .background(Color(.systemGray5))
+        .cornerRadius(8)
+
+        VStack(alignment: .leading, spacing: 10) {
+          Text(noteArticle.title)
+            .font(.system(.headline))
+          Text(noteArticle.author)
+            .font(.system(.caption))
         }
 
-        HStack {
-          Group {
-            if let eyeCatchImageURL = noteArticle.eyeCatchImageURL,
-               let url = URL(string: eyeCatchImageURL) {
-              AsyncImage(url: url) { image in
-                image
-                  .resizable()
-                  .scaledToFill()
-              } placeholder: {
-                ProgressView()
-              }
-            } else {
-              Image(systemName: "photo")
-            }
-          }
-          .frame(width: 60, height: 60)
-          .background(Color(.systemGray5))
-          .cornerRadius(8)
+        Spacer()
 
-          VStack(alignment: .leading, spacing: 10) {
-            Text(noteArticle.title)
-              .font(.system(.headline))
-            Text(noteArticle.author)
-              .font(.system(.caption))
-          }
-          .frame(maxWidth: .infinity)
-
-          Spacer()
-
-          HStack(spacing: 12) {
-            if loader.loadingArticle != nil {
-              ProgressView()
-                .frame(width: 28, height: 28)
-                .foregroundColor(.black)
-                .padding()
-            } else if player.playingArticle == article {
-              Button {
-                player.stop()
-              } label: {
-                Image(systemName: "stop.fill")
-                  .frame(width: 28, height: 28)
-                  .foregroundColor(.black)
-                  .padding()
-              }
-            } else {
-              Button {
-                loader.load(article: article)
-              } label: {
-                Image(systemName: "play.fill")
-                  .frame(width: 28, height: 28)
-                  .foregroundColor(.black)
-                  .padding()
-              }
-            }
-
-            NavigationLink {
-              ArticleWebViewPage(article: article)
+        HStack(spacing: 4) {
+          if loader.loadingArticle != nil {
+            ProgressView()
+              .frame(width: 14, height: 14)
+              .foregroundColor(.black)
+              .padding()
+          } else if player.playingArticle == article {
+            Button {
+              player.stop()
             } label: {
-              Image(systemName: "safari")
-                .frame(width: 28, height: 28)
+              Image(systemName: "stop.fill")
+              .frame(width: 14, height: 14)
                 .foregroundColor(.black)
                 .padding()
             }
+          } else {
+            Button {
+              loader.load(article: article)
+            } label: {
+              Image(systemName: "play.fill")
+              .frame(width: 14, height: 14)
+                .foregroundColor(.black)
+                .padding()
+            }
+          }
+
+          NavigationLink {
+            ArticleWebViewPage(article: article)
+          } label: {
+            Image(systemName: "safari")
+              .frame(width: 14, height: 14)
+              .foregroundColor(.black)
+              .padding()
           }
         }
       }
