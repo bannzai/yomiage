865732575c3a3b4a8c077dfbf1cac2cb4415a75a
bannzai <kingkong999yhirose@gmail.com>
2022-05-03 12:58:23 +0900

    Add AppStorageKey extension

---
+++ Yomiage/StdLib/AppStorage+.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+enum AppStorageKeys {
+  static let playerVolume = "playerVolume"
+  static let playerPitch = "playerPitch"
+  static let playerRate = "playerRate"
+}
+
+typealias UserDefaultsKeys = AppStorageKeys
