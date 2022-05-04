4d25f8688855e833ffecc4430f7ef7c7e66af303
bannzai <kingkong999yhirose@gmail.com>
2022-05-04 18:29:26 +0900

    Remove unnecessary code

--- Yomiage/StdLib/String+.swift
+++ Yomiage/StdLib/String+.swift
@@ -1,11 +1,5 @@
 import Foundation
 
-extension String {
-  var trimmed: String {
-    trimmingCharacters(in: .whitespacesAndNewlines)
-  }
-}
-
 extension String: LocalizedError {
   public var errorDescription: String? { "エラーが発生しました" }
   public var failureReason: String? { self }
