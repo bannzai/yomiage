4d9b8481deab7fd9f26d0b3d53410553c746ade6
bannzai <kingkong999yhirose@gmail.com>
2022-05-04 09:37:05 +0900

    Embed on ZStack

--- Yomiage/Features/Articles/NoteArticle.swift
+++ Yomiage/Features/Articles/NoteArticle.swift
@@ -9,78 +9,84 @@ struct NoteArticle: View {
 
   var body: some View {
     if let noteArticle = noteArticle {
-      HStack {
-        Group {
-          if let eyeCatchImageURL = noteArticle.eyeCatchImageURL,
-             let url = URL(string: eyeCatchImageURL) {
-            AsyncImage(url: url) { image in
-              image
-                .resizable()
-                .scaledToFill()
-            } placeholder: {
+      ZStack {
+        if let article = loader.loadingArticle?.note, let url = URL(string: article.pageURL) {
+          LoadHTMLWebView(url: url, loader: loader)
+        }
+
+        HStack {
+          Group {
+            if let eyeCatchImageURL = noteArticle.eyeCatchImageURL,
+               let url = URL(string: eyeCatchImageURL) {
+              AsyncImage(url: url) { image in
+                image
+                  .resizable()
+                  .scaledToFill()
+              } placeholder: {
+                ProgressView()
+              }
+            } else {
+              Image(systemName: "photo")
+            }
+          }
+          .frame(width: 60, height: 60)
+          .background(Color(.systemGray5))
+          .cornerRadius(8)
+
+          VStack(alignment: .leading, spacing: 10) {
+            Text(noteArticle.title)
+              .font(.system(.headline))
+            Text(noteArticle.author)
+              .font(.system(.caption))
+          }
+
+          Spacer()
+
+          HStack(spacing: 4) {
+            if loader.loadingArticle != nil {
               ProgressView()
-            }
-          } else {
-            Image(systemName: "photo")
-          }
-        }
-        .frame(width: 60, height: 60)
-        .background(Color(.systemGray5))
-        .cornerRadius(8)
-
-        VStack(alignment: .leading, spacing: 10) {
-          Text(noteArticle.title)
-            .font(.system(.headline))
-          Text(noteArticle.author)
-            .font(.system(.caption))
-        }
-
-        Spacer()
-
-        HStack(spacing: 4) {
-          if loader.loadingArticle != nil {
-            ProgressView()
-              .frame(width: 14, height: 14)
-              .foregroundColor(.black)
-              .padding()
-          } else if player.playingArticle == article {
-            Button {
-              player.stop()
-            } label: {
-              Image(systemName: "stop.fill")
-              .frame(width: 14, height: 14)
+                .frame(width: 14, height: 14)
                 .foregroundColor(.black)
                 .padding()
+            } else if player.playingArticle == article {
+              Button {
+                player.stop()
+              } label: {
+                Image(systemName: "stop.fill")
+                .frame(width: 14, height: 14)
+                  .foregroundColor(.black)
+                  .padding()
+              }
+            } else {
+              Button {
+                loader.load(article: article)
+              } label: {
+                Image(systemName: "play.fill")
+                .frame(width: 14, height: 14)
+                  .foregroundColor(.black)
+                  .padding()
+              }
             }
-          } else {
-            Button {
-              loader.load(article: article)
+
+            NavigationLink {
+              ArticleWebViewPage(article: article)
             } label: {
-              Image(systemName: "play.fill")
-              .frame(width: 14, height: 14)
+              Image(systemName: "safari")
+                .frame(width: 14, height: 14)
                 .foregroundColor(.black)
                 .padding()
             }
           }
-
-          NavigationLink {
-            ArticleWebViewPage(article: article)
-          } label: {
-            Image(systemName: "safari")
-              .frame(width: 14, height: 14)
-              .foregroundColor(.black)
-              .padding()
-          }
         }
+        .padding()
+        .errorAlert(error: $loader.localizedError)
+        .onReceive(loader.$loadedBody) { body in
+          guard let body = body else {
+            return
+          }
+
+          player.speak(article: article, text: body)
       }
-      .padding()
-      .errorAlert(error: $loader.localizedError)
-      .onReceive(loader.$loadedBody) { body in
-        guard let body = body else {
-          return
-        }
-
-        player.speak(article: article, text: body)
       }
     }
   }
