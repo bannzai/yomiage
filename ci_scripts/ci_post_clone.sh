c4f418ee8fe52d78ae87e491ead7dbc03f7fad9f
bannzai <kingkong999yhirose@gmail.com>
2022-05-07 08:13:17 +0900

    Remove unnecessary phase

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,9 +7,3 @@ REPOSITORY_DIR="$(cd $SCRIPT_DIR && cd .. && pwd -P)"
 cd $REPOSITORY_DIR
 make secret
 cd -
-
-echo "Log: BUNDLE IDENTIFIER: $CI_BUNDLE_ID"
-if [[ $CI_BUNDLE_ID = 'com.bannzai.yomiage.dev' ]]; then
-  echo "Log: Replace Release.xcconfig"
-  sed -i '' -e 's/Production.xcconfig/Development.xcconfig/g' $REPOSITORY_DIR/Yomiage/Config/Release.xcconfig
-fi
