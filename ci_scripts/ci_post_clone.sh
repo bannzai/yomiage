ed89224df2140ccdf5330e494395d16a5ced3b93
bannzai <kingkong999yhirose@gmail.com>
2022-05-06 20:47:47 +0900

    add make secret

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+make secret
+
 if [[ $CI_BUNDLE_ID = 'com.bannzai.yomiage.dev' ]]; then
   sed -i 's/"Production.xcconfig"/"Development.xcconfig"/g' Yomiage/Config/Release.xcocnfig
 fi
