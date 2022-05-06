5ace2697c8f6f00fa5849870627e0125c3671d27
bannzai <kingkong999yhirose@gmail.com>
2022-05-06 20:57:42 +0900

    Replace to Abstract URL

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -eu
 
+SCRIPT_DIR="$(cd `dirname $0` && pwd -P)"
+REPOSITORY_DIR="$(cd $SCRIPT_DIR && cd .. && pwd -P)"
+
+
 make secret
 
 if [[ $CI_BUNDLE_ID = 'com.bannzai.yomiage.dev' ]]; then
-  sed -i 's/"Production.xcconfig"/"Development.xcconfig"/g' Yomiage/Config/Release.xcocnfig
+  sed -i 's/"Production.xcconfig"/"Development.xcconfig"/g' $REPOSITORY_DIR/Yomiage/Config/Release.xcocnfig
 fi
