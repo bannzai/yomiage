2b8c959781058b8e0d56b481ea44d18d14ebe502
bannzai <kingkong999yhirose@gmail.com>
2022-05-06 21:01:09 +0900

    cd REPOSITORY_DIR

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,7 +5,9 @@ SCRIPT_DIR="$(cd `dirname $0` && pwd -P)"
 REPOSITORY_DIR="$(cd $SCRIPT_DIR && cd .. && pwd -P)"
 
 
+cd $REPOSITORY_DIR
 make secret
+cd -
 
 if [[ $CI_BUNDLE_ID = 'com.bannzai.yomiage.dev' ]]; then
   sed -i 's/"Production.xcconfig"/"Development.xcconfig"/g' $REPOSITORY_DIR/Yomiage/Config/Release.xcocnfig
