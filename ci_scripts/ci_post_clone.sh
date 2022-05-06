f5c1ed7934000ee880e9b33ec76888575eb67499
bannzai <kingkong999yhirose@gmail.com>
2022-05-06 20:53:02 +0900

    Revert "add make secret"
    This reverts commit ed89224df2140ccdf5330e494395d16a5ced3b93.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eu
 
-make secret
-
 if [[ $CI_BUNDLE_ID = 'com.bannzai.yomiage.dev' ]]; then
   sed -i 's/"Production.xcconfig"/"Development.xcconfig"/g' Yomiage/Config/Release.xcocnfig
 fi
