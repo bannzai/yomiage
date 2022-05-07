171477a9390e5efb3978b2a6a3bcbb2a2faed2aa
bannzai <kingkong999yhirose@gmail.com>
2022-05-07 17:26:13 +0900

    Add timeout

---
+++ Yomiage/StdLib/Timeout.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+public func timeout<T>(
+  seconds: Foundation.TimeInterval,
+  operation: @escaping @Sendable () async throws -> T
+) async throws -> T {
+  try await withThrowingTaskGroup(of: T.self) { group in
+    group.addTask {
+      try await operation()
+    }
+
+    group.addTask {
+      if seconds > 0 {
+        try await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
+      }
+      try Task.checkCancellation()
+      throw TimedOutError()
+    }
+
+    defer{
+      group.cancelAll()
+    }
+    
+    do {
+      let result = try await group.next()!
+      return result
+    } catch {
+      print("[DEBUG]", "error: \(error)")
+      throw error
+    }
+  }
+}
+
+private struct TimedOutError: LocalizedError {
+  var errorDescription: String? {
+    "タイムアウトしました"
+  }
+  var failureReason: String? {
+    "一定時間が経過したため処理を中断しました。再度お試しください"
+  }
+  var recoverySuggestion: String? = nil
+  var helpAnchor: String? = nil
+}
