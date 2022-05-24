34f1de3bd83520103d641edc31195a97d0fc52e9
bannzai <kingkong999yhirose@gmail.com>
2022-05-24 21:31:34 +0900

    Prepare playerbar

--- Yomiage/Features/Player/PlayerBar.swift
+++ Yomiage/Features/Player/PlayerBar.swift
@@ -3,8 +3,64 @@ import SwiftUI
 struct PlayerBar: View {
   @EnvironmentObject var player: Player
 
+  let article: Article
+
   var body: some View {
-    EmptyView()
+    VStack {
+      if let title = title {
+        Text(title)
+          .font(.subheadline.weight(.medium))
+      }
+
+      HStack {
+        Button {
+          analytics.logEvent("player_bar_play_button_pressed", parameters: ["article_id": String(describing: article.id)])
+
+          player.stop()
+        } label: {
+          Image(systemName: "backward.frame.fill")
+            .font(.body.weight(.heavy))
+            .foregroundColor(.label)
+            .padding()
+        }
+        Spacer()
+
+        Button {
+          analytics.logEvent("player_bar_play_button_pressed", parameters: ["article_id": String(describing: article.id)])
+
+          player.stop()
+        } label: {
+          Image(systemName: "stop.fill")
+            .font(.body.weight(.heavy))
+            .foregroundColor(.label)
+            .padding()
+        }
+        Spacer()
+
+        Button {
+          analytics.logEvent("player_bar_play_button_pressed", parameters: ["article_id": String(describing: article.id)])
+
+          player.stop()
+        } label: {
+          Image(systemName: "forward.frame.fill")
+            .font(.body.weight(.heavy))
+            .foregroundColor(.label)
+            .padding()
+        }
+      }
+      .padding()
+    }
+  }
+
+  private var title: String? {
+    switch article.kindWithValue {
+    case let .note(note):
+      return note.title
+    case let .medium(medium):
+      return medium.title
+    case nil:
+      return nil
+    }
   }
 }
 
