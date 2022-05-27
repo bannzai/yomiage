3e5795f39dc77fee70c134534acb51dbf14784cd
bannzai <kingkong999yhirose@gmail.com>
2022-05-27 22:10:04 +0900

    Fix app other info page

--- Yomiage/Features/AppOtherSetting/AppOtherSettingPage.swift
+++ Yomiage/Features/AppOtherSetting/AppOtherSettingPage.swift
@@ -5,13 +5,25 @@ struct AppOtherSettingPage: View {
     List {
       Section("アプリについて") {
         NavigationLink {
-          SafariView(url: .init(string: "https://bannzai.github.io/yomiage/Terms")!)
+          WebView(url: .init(string: "https://docs.google.com/forms/d/e/1FAIpQLSdE-Dr37Gr8_tPUsF1HBpZroJPXpp4AcDl6YkbaQ4shJ4SCRw/viewform?usp=sf_link")!)
+            .navigationTitle("お問い合わせ")
+            .navigationBarTitleDisplayMode(.inline)
+        } label: {
+          Text("お問い合わせ")
+        }
+
+        NavigationLink {
+          WebView(url: .init(string: "https://bannzai.github.io/yomiage/Terms")!)
+            .navigationTitle("利用規約")
+            .navigationBarTitleDisplayMode(.inline)
         } label: {
           Text("利用規約")
         }
 
         NavigationLink {
-          SafariView(url: .init(string: "https://bannzai.github.io/yomiage/PrivacyPolicy")!)
+          WebView(url: .init(string: "https://bannzai.github.io/yomiage/PrivacyPolicy")!)
+            .navigationTitle("プライバシーポリシー")
+            .navigationBarTitleDisplayMode(.inline)
         } label: {
           Text("プライバシーポリシー")
         }
