69b24fc90a658a98304e8ad95047e0b5895baa06
bannzai <kingkong999yhirose@gmail.com>
2022-05-30 08:59:37 +0900

    Use pipe

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -6,7 +6,7 @@ REPOSITORY_DIR="$(cd $SCRIPT_DIR && cd .. && pwd -P)"
 
 # Can not use separators [/, :, @]
 cat $REPOSITORY_DIR/Yomiage/Config/Secret.swift.sample | sed \
-  -e "s;[GOOGLE_TEXT_TO_SPEECH_API_KEY];$GOOGLE_TEXT_TO_SPEECH_API_KEY;g" \
+  -e "s|[GOOGLE_TEXT_TO_SPEECH_API_KEY]|$GOOGLE_TEXT_TO_SPEECH_API_KEY|g" \
 > $REPOSITORY_DIR/Yomiage/Config/Secret.swift
 
 
