7e2bb5ea7905608b2de54935d8aad22436c6b18e
bannzai <kingkong999yhirose@gmail.com>
2022-05-30 09:07:46 +0900

    fix secret command

--- scripts/secret.sh
+++ scripts/secret.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euo pipefail
+set -x
 
 SCRIPT_DIR="$(cd `dirname $0` && pwd -P)"
 REPOSITORY_DIR="$(cd $SCRIPT_DIR && cd .. && pwd -P)"
 
 # Can not use separators [/, :, @]
 cat $REPOSITORY_DIR/Yomiage/Config/Secret.swift.sample | sed \
-  -i '' -e "s|[GOOGLE_TEXT_TO_SPEECH_API_KEY]|$GOOGLE_TEXT_TO_SPEECH_API_KEY|g" \
+   -e "s|\[GOOGLE_TEXT_TO_SPEECH_API_KEY\]|$GOOGLE_TEXT_TO_SPEECH_API_KEY|g" \
 > $REPOSITORY_DIR/Yomiage/Config/Secret.swift
 
-
+echo
 
