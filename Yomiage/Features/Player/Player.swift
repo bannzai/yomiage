9dfcf0972e428d5862295b3bac0ae5b95b77c04b
bannzai <kingkong999yhirose@gmail.com>
2022-06-01 18:18:19 +0900

    Revert "Use mainMixer outputFormat"
    This reverts commit a99a025e78215658620d640a0a75c48d3a7cfb0d.

--- Yomiage/Features/Player/Player.swift
+++ Yomiage/Features/Player/Player.swift
@@ -15,7 +15,7 @@ final class Player: NSObject, ObservableObject {
 
   private let audioEngine = AVAudioEngine()
   private let playerNode = AVAudioPlayerNode()
-  private var outputAudioFormat: AVAudioFormat!
+  private let outputAudioFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 22050, channels: 1, interleaved: false)!
 
   private let synthesizer = AVSpeechSynthesizer()
   private var canceller: Set<AnyCancellable> = []
@@ -48,7 +48,6 @@ final class Player: NSObject, ObservableObject {
 
     synthesizer.delegate = self
 
-    outputAudioFormat = audioEngine.mainMixerNode.outputFormat(forBus: 0)
     audioEngine.attach(playerNode)
     audioEngine.connect(playerNode, to: audioEngine.mainMixerNode, format: outputAudioFormat)
     audioEngine.prepare()
