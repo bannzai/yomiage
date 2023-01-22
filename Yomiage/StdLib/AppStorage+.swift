6c43ebb276c46e8faee99321f639eb0f8aadbb73
bannzai <kingkong999yhirose@gmail.com>
2023-01-22 15:17:08 +0900

    Replace to UserDefaults

--- Yomiage/StdLib/AppStorage+.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-enum AppStorageKeys {
-  static let playerVolume = "playerVolume"
-  static let playerPitch = "playerPitch"
-  static let playerRate = "playerRate"
-}
-
-typealias UserDefaultsKeys = AppStorageKeys
