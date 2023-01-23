4a2f36b00d842c07c3ee25ea27277c464dbf8221
bannzai <kingkong999yhirose@gmail.com>
2023-01-23 21:43:17 +0900

    Add synthesizer

--- Yomiage/Features/PlayerSetting/PlayerSettingSheet.swift
+++ Yomiage/Features/PlayerSetting/PlayerSettingSheet.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct PlayerSettingSheet: View {
   @Environment(\.dismiss) private var dismiss
-  @EnvironmentObject var player: Synthesizer
+  @EnvironmentObject var synthesizer: Synthesizer
 
   var body: some View {
     MediumSheetLayout {
@@ -13,19 +13,19 @@ struct PlayerSettingSheet: View {
         VStack(spacing: 20) {
           VStack(alignment: .leading, spacing: 8) {
             Text("音量").font(.system(.subheadline))
-            Slider(value: $player.volume, in: Range.volume)
+            Slider(value: $synthesizer.volume, in: Range.volume)
           }
           VStack(alignment: .leading, spacing: 8) {
             Text("読む早さ").font(.system(.subheadline))
-            Slider(value: $player.rate, in: Range.rate)
+            Slider(value: $synthesizer.rate, in: Range.rate)
           }
           VStack(alignment: .leading, spacing: 8) {
             Text("声の高さ").font(.system(.subheadline))
-            Slider(value: $player.pitch, in: Range.pitch)
+            Slider(value: $synthesizer.pitch, in: Range.pitch)
           }
 
           Button {
-            player.test()
+            synthesizer.test()
           } label: {
             Text("テスト")
               .padding()
