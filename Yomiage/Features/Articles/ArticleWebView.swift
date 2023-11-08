519b45354b172e97896a52cfc56d62968ebfc0e7
bannzai <kingkong999yhirose@gmail.com>
2023-11-08 13:12:22 +0900

    Fix webView

--- Yomiage/Features/Articles/ArticleWebView.swift
+++ Yomiage/Features/Articles/ArticleWebView.swift
@@ -6,21 +6,10 @@ struct ArticleWebViewPage: View {
   let article: Article
 
   var body: some View {
-    switch article.typedKind {
-    case .note:
-      if let note = article.note, let url = URL(string: article.pageURL) {
-        ArticleWebView(url: url)
-          .navigationTitle(note.title)
-          .navigationBarTitleDisplayMode(.inline)
-      }
-    case .medium:
-      if let medium = article.medium, let url = URL(string: article.pageURL) {
-        ArticleWebView(url: url)
-          .navigationTitle(medium.title)
-          .navigationBarTitleDisplayMode(.inline)
-      }
-    case nil:
-      EmptyView()
+    if let url = URL(string: article.pageURL) {
+      ArticleWebView(url: url)
+        .navigationTitle(article.title)
+        .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
