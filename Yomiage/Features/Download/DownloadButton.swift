5be82265386a4fd51ec0e402a9937cbe11b55aa6
bannzai <kingkong999yhirose@gmail.com>
2023-11-08 13:32:34 +0900

    Fix

--- Yomiage/Features/Download/DownloadButton.swift
+++ Yomiage/Features/Download/DownloadButton.swift
@@ -8,14 +8,14 @@ struct DownloadButton: View {
   @State private var isDownloading = false
 
   var body: some View {
-    if let pageURL = URL(string: article.pageURL), let kind = article.typedKind {
+    if let pageURL = URL(string: article.pageURL) {
       Button {
         isDownloading = true
         Task { @MainActor in
           do {
             let html = try await loadHTML(url: pageURL)
             let htmlToSSML = try await functions.htmlToSSML(url: pageURL, html: html)
-            _ = try await synthesizer.writeToAudioFile(body: htmlToSSML.ssml, pageURL: pageURL)
+            _ = try await synthesizer.writeToAudioFile(htmlToSSML: htmlToSSML, pageURL: pageURL)
           } catch {
             self.error = error
           }
