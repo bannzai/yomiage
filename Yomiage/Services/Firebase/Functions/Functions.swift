3ed8874a732aed540a6b0fe1df2e565122e27c43
bannzai <kingkong999yhirose@gmail.com>
2023-11-08 11:29:46 +0900

    Add functions

--- Yomiage/Services/Firebase/Functions/Functions.swift
+++ Yomiage/Services/Firebase/Functions/Functions.swift
@@ -1,1 +1,62 @@
 import Foundation
+import FirebaseFunctions
+
+let functions = FirebaseFunctions.Functions.functions(region: "asia-northeast1")
+
+// MARK: - html_to_ssml
+extension FirebaseFunctions.Functions {
+  struct HTMLToSSML: Codable {
+    let ssml: String
+    let contentBlocks: [ContentBlock]
+
+    struct ContentBlock: Codable {
+      let title: String
+      let content: String
+    }
+  }
+  func htmlToSSML(html: String) async throws -> HTMLToSSML {
+    let result = try await httpsCallable("html_to_ssml").call(["html": html])
+    return try result.decode()
+  }
+}
+
+// MARK: - fileprivate
+fileprivate extension HTTPSCallableResult {
+  var json: [String: Any]? {
+    if let result = data as? [String: Any], let d = result["data"] as? [String: Any] {
+      return d
+    } else {
+      return nil
+    }
+  }
+
+  enum DecodeError: LocalizedError {
+    case jsonDataIsNotFound
+    case jsonDataisInvalidFormat
+
+    var errorDescription: String? {
+      switch self {
+      case .jsonDataIsNotFound: "JSON data is not found"
+      case .jsonDataisInvalidFormat: "JSON is invalid format"
+      }
+    }
+    var failureReason: String? {
+      switch self {
+      case .jsonDataIsNotFound: "Please check your internet connection and retry after"
+      case .jsonDataisInvalidFormat: "Please check your internet connection and retry after"
+      }
+    }
+  }
+
+  func decode<T>() throws -> T where T: Decodable {
+    guard let json else {
+      throw DecodeError.jsonDataIsNotFound
+    }
+    if JSONSerialization.isValidJSONObject(json) {
+      throw DecodeError.jsonDataisInvalidFormat
+    }
+    let data = try JSONSerialization.data (withJSONObject: json, options: [])
+    return try JSONDecoder().decode(T.self, from: data)
+  }
+}
+
